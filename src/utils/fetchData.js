2c35aacee331ff706ff4af73d0e5c443488684c0
Bongumenzi2724 <bongumenzinzama@gmail.com>
2022-09-08 02:08:09 +0200

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key':'1211681d68msh89ee39fd9819839p1b0d07jsn25a94f452550',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key':'1211681d68msh89ee39fd9819839p1b0d07jsn25a94f452550',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
